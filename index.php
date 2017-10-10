ed329001bf94d960967653de7a11795610e54a99
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-10 19:51:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo <h1>"Let the sun shine for you!"</h1>;
 echo <p>"How do you feel now?"</p>;
-echo <p><a href'http://burnedpixel.com/blog/setting-up-git-and-github-on-your-mac/#gitidentification"></a></p>;
+echo <p><a href = 'http://burnedpixel.com/blog/setting-up-git-and-github-on-your-mac/#gitidentification"></p>;
